ef062e42437eb65d1fdd8a1526d7649632eb1a64
Divyanshu Bhaskar <thedivyanshubhaskar@gmail.com>
2021-12-01 01:58:15 +0530

    Project_01: Band Name Generator

---
+++ Day_01/Project: Band Name Generator/Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome!.. To the Band Name Generator\n")
+city = input("Enter the name of the city you grew up in\n")
+pet =input("Enter the name of your first pet\n")
+print("\nGenerated band name:\n" + city + " " + pet)
